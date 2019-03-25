1414de3e91819705cb1274fdf692a8452eefa194
unknown <davidvuletas@uns.ac.rs>
2019-03-25 20:17:30 +0100

    Dockerized

--- uks_project/wsgi.py
+++ uks_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for my_git project.
+WSGI config for uks_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_git.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uks_project.settings')
 
 application = get_wsgi_application()
