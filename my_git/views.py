23fb6fc34d8be143dadd918973a18b330dd9d2fa
Marija <marijakovacevic995@gmail.com>
2019-03-31 21:36:32 +0200

    Resolved conflicts

--- my_git/views.py
+++ my_git/views.py
@@ -3,20 +3,38 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from my_git.forms import UserRegisterForm
+from my_git.forms import UserRegisterForm, LoginForm
 from my_git.models import User
+from my_git.constants import HttpMethod
 
 
 def welcome(request):
     return render(request, 'my_git/welcome.html')
 
+
 def home(request):
     return render(request, 'my_git/home.html')
 
-# todo: create login function
+
+def login(request):
+    print(request.method)
+    if request.method == HttpMethod.POST.name:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            try:
+                result = User.objects.get(email=form.cleaned_data['email'], password=form.cleaned_data['password'])
+                print(result)
+                return redirect('home')
+            except User.DoesNotExist:
+                messages.error(request, 'Bad credentials, try to login now!')
+                return redirect('login')
+    else:
+        form = LoginForm()
+    return render(request, 'my_git/users/login.html', {'form': form})
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == HttpMethod.POST.name:
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             obj = User()
@@ -28,4 +46,4 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'my_git/users/register.html', {'form':form})
+    return render(request, 'my_git/users/register.html', {'form': form})
