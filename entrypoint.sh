5aa093c9a70a4d91f94ef70f4d8791fdc035dab8
davidvuletas <davidvuletas@uns.ac.rs>
2019-04-23 20:02:37 +0200

    Added metrics packages (telegraf, grafana, influxdb) #38

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ python manage.py makemigrations
 # apply migrations
 python manage.py migrate
 
+# create super user
+python manage.py createsuperuser --username admin --email admin@example.com --noinput
+python manage.py changepassword admin
 # start server
 python manage.py runserver 0.0.0.0:8000
